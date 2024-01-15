429bb05058ac89356ec5e8e5fbb77df1cc9129fa
Harsh chaudhary <52584027+protocolblack3302@users.noreply.github.com>
2024-01-15 17:53:27 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-.xod
-are being treated as zip in zscaler
-so user is not able to view the document
-this is actually not a zip file so , we cant ask user to raise a zip catalog request.
+print("harsh")
